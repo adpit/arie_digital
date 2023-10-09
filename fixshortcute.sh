84865152151bd706892638525641007e8382dec0
adpit <adpit31@gmail.com>
2023-10-09 09:44:39 +0700

    Add templates for missing shortcodes

--- fixshortcute.sh
+++ fixshortcute.sh
@@ -2,6 +2,9 @@
 unique_shortcodes=$(grep -r -o -E "{{%|{{< [a-zA-Z0-9_-]+" ./content | awk '{print $2}' | sort -u)
 commit_flag=false  # Tanda untuk mengecek apakah ada file yang harus di-commit
 
+# Array khusus untuk shortcode yang tidak ada dalam daftar "children"
+missing_shortcodes=("children")
+
 # Iterasi melalui setiap shortcode dan periksa apakah file template ada
 for shortcode in $unique_shortcodes; do
   template_path="./layouts/shortcodes/$shortcode.html"
@@ -9,7 +12,26 @@ for shortcode in $unique_shortcodes; do
   if [[ ! -f "$template_path" ]]; then
     # Jika file tidak ada, buat file template sederhana
     echo "<!-- Template for $shortcode -->" > $template_path
+    echo "<a href=\"{{ .Get 0 }}\">{{ .Inner }}</a>" >> $template_path
     echo "File template untuk shortcode '$shortcode' telah dibuat."
+
+    # Tandai bahwa ada file yang perlu di-commit
+    commit_flag=true
+
+    # Tambahkan ke Git staging area
+    git add $template_path
+  fi
+done
+
+# Iterasi melalui shortcode yang tidak ada dalam daftar "children"
+for missing_shortcode in "${missing_shortcodes[@]}"; do
+  template_path="./layouts/shortcodes/$missing_shortcode.html"
+
+  if [[ ! -f "$template_path" ]]; then
+    # Jika file tidak ada, buat file template sederhana
+    echo "<!-- Template for $missing_shortcode -->" > $template_path
+    echo "<a href=\"{{ .Get 0 }}\">{{ .Inner }}</a>" >> $template_path
+    echo "File template untuk shortcode '$missing_shortcode' telah dibuat."
 
     # Tandai bahwa ada file yang perlu di-commit
     commit_flag=true
@@ -23,4 +45,3 @@ done
 if [ "$commit_flag" = true ]; then
   git commit -m "Add templates for missing shortcodes"
 fi
-
